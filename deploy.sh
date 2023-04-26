c276b44a092707b0d59ede4a62bbf9bd26b65116
Ben (X13/Arch) <ben@wolkenwelten.net>
2023-04-26 18:58:33 +0200

    Updates

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm -rf deploy/ && ./main.nuj && rsync -avhe ssh deploy/ wolkenwelten.net:/home/nujel/nujel.net/
+rm -rf deploy/ && ./main.nuj && rsync -avhe ssh deploy/ wolkenwelten.net:/srv/http/nujel.net/
